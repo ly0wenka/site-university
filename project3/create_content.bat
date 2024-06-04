89c8d93d68c83d2a7dd01afeb774af0bc962d62b
ly0wenka <ly0wenka@gmail.com>
2024-06-04 15:44:44 +0300

    3

---
+++ project3/create_content.bat
@@ -0,0 +1,27 @@
+@echo off
+REM Create directories
+
+mkdir content
+mkdir content\images
+
+REM Create placeholder files for images
+
+type nul > content\images\khpi-logo.png
+type nul > content\images\khpi-campus.jpg
+type nul > content\images\event1.jpg
+type nul > content\images\event2.jpg
+type nul > content\images\event3.jpg
+type nul > content\images\lab1.jpg
+type nul > content\images\lab2.jpg
+type nul > content\images\lab3.jpg
+type nul > content\images\student1.jpg
+type nul > content\images\student2.jpg
+type nul > content\images\student3.jpg
+type nul > content\images\student4.jpg
+type nul > content\images\student5.jpg
+type nul > content\images\sports.jpg
+type nul > content\images\campus.png
+type nul > content\images\clubs.jpg
+
+echo Directories and placeholder image files created successfully.
+
